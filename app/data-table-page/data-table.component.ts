b1232f7b6cff6fa5e80e8319f19c3ad953dc9536
Marcin Malinowski <orientman@gmail.com>
2016-10-19 14:57:39 +0200

    are generics any better?

--- app/data-table-page/data-table.component.ts
+++ app/data-table-page/data-table.component.ts
@@ -4,15 +4,15 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
   selector: 'data-table',
   templateUrl: "app/data-table-page/data-table.component.html"
 })
-export class DataTableComponent {
-    @Input() items: any[];
+export class DataTableComponent<T> {
+    @Input() items: T[];
     @Input() headers: Object[];
-    @Input() selectedItem: any;
-    @Output() itemClick = new EventEmitter<any>();
+    @Input() selectedItem: T;
+    @Output() itemClick = new EventEmitter<T>();
 
     constructor() { }
 
-    onItemClick(item: any) {
+    onItemClick(item: T) {
         console.log('Selecting...');
         this.selectedItem = item;
         this.itemClick.emit(item);
