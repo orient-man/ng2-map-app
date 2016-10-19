1155562bfb752fc20277b0604d407b4928efc3ac
Marcin Malinowski <orientman@gmail.com>
2016-10-19 14:44:16 +0200

    selecting item

--- app/data-table-page/data-table.component.ts
+++ app/data-table-page/data-table.component.ts
@@ -1,12 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'data-table',
   templateUrl: "app/data-table-page/data-table.component.html"
 })
 export class DataTableComponent {
-  @Input() items: any[];
-  @Input() headers: Object[];
+    @Input() items: any[];
+    @Input() headers: Object[];
+    @Input() selectedItem: any;
+    @Output() itemClick = new EventEmitter<any>();
 
-  constructor() { }
+    constructor() { }
+
+    onItemClick(item: any) {
+        console.log('Selecting...');
+        this.selectedItem = item;
+        this.itemClick.emit(item);
+    }
 }
